8d3dfe4162270eb1fa575c2dbe12bdb4925f2590
Jeff Nyman <jeffnyman@gmail.com>
2021-04-04 09:11:48 -0500

    Sanity test for Mocha-style formatting.

---
+++ cypress/e2e/pedagogical/simple.spec.js
@@ -0,0 +1,9 @@
+describe("Simple Spec Pedagogy", () => {
+  it("recognizes that truth is true", () => {
+    expect(true).to.equal(true)
+  })
+
+  it("understands falsity cannot be true", () => {
+    expect(false).to.not.equal(true)
+  })
+})
